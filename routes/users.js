efeeeefbda43580ce5d825bdb5e4bfa91a4cf5c4
Alexandr Metreniuc <metreniuk@gmail.com>
2025-04-10 12:19:29 +0300

    update user route

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  return res.json({ message: "hello world, from the server" });
+  return res.json({ message: "hello world, from: " + Math.random() * 100 });
 });
 
 router.get("/test", function (req, res, next) {
